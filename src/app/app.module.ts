39d8099bc5d3c119d03692c000124e30ed1e1e24
Will Forbes <wforbes87@gmail.com>
2023-06-03 10:41:01 -0700

    import new modules to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeModule } from './home/home.module';
+import { ContactModule } from './contact/contact.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ServicesModule } from './services/services.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HomeModule,
+    ContactModule,
+    ProjectsModule,
+    ServicesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
