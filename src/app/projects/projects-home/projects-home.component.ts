f6cd2437b194ef9032a2d4c618061f1eb971ee72
Will Forbes <wforbes87@gmail.com>
2023-06-03 10:40:00 -0700

    add projects module and component
    using 'ng generate module' and '..component' commands

---
+++ src/app/projects/projects-home/projects-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-home',
+  templateUrl: './projects-home.component.html',
+  styleUrls: ['./projects-home.component.css']
+})
+export class ProjectsHomeComponent {
+
+}
