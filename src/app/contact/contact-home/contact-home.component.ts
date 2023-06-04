6fe33f86872cbd0594bdebfe69c8bddfa24a447a
Will Forbes <wforbes87@gmail.com>
2023-06-03 23:55:33 -0700

    contact form data binding
    - bind data to form fields
    - disable send button until at least one field has text in it

--- src/app/contact/contact-home/contact-home.component.ts
+++ src/app/contact/contact-home/contact-home.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck, SimpleChanges } from '@angular/core';
+
+type ContactForm = {
+  name: string | null;
+  email: string | null;
+  phone: string | null;
+  message: string | null;
+};
 
 @Component({
   selector: 'app-contact-home',
   templateUrl: './contact-home.component.html',
   styleUrls: ['./contact-home.component.css']
 })
-export class ContactHomeComponent {
+export class ContactHomeComponent implements OnInit, DoCheck {
+  isUnchanged = true;
+  name= '';
+  contactForm: ContactForm = {
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  };
 
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngDoCheck(): void {
+    this.isUnchanged = (
+      this.contactForm.name === '' &&
+      this.contactForm.email === '' &&
+      this.contactForm.phone === '' &&
+      this.contactForm.message === ''
+    )
+  }
+
+  getEventValue(event: KeyboardEvent) {
+    return (event.target as HTMLInputElement).value;
+  }
 }
