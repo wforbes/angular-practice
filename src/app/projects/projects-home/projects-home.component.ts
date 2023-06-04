7c0285b91ad97220318040d02a969e68f19d1025
Will Forbes <wforbes87@gmail.com>
2023-06-04 13:09:17 -0700

    add Dynamic Business Card project
    - used WGU course material code as starting point
    - adding all fields from prior course material example into this dynamic
    - added font size variations for first characters that are present in prior example using Angulars iterative and conditional rendering
    - restyled the Modify form to align labels and fields better

--- src/app/projects/projects-home/projects-home.component.ts
+++ src/app/projects/projects-home/projects-home.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+type CardForm = {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  company: string;
+  title: string;
+  address: {
+    street: string;
+    city: string;
+    state: string;
+    zip: string;
+  };
+};
 
 @Component({
   selector: 'app-projects-home',
   templateUrl: './projects-home.component.html',
   styleUrls: ['./projects-home.component.css']
 })
-export class ProjectsHomeComponent {
+export class ProjectsHomeComponent implements OnInit {
+  cardForm: CardForm = {
+    firstName: 'William',
+    lastName: 'Forbes',
+    phone: '(201) 748-6000',
+    company: 'Recursive Code',
+    title: 'Software Engineer',
+    address: {
+      street: '1234 Fake St',
+      city: 'Los Angeles',
+      state: 'CA',
+      zip: '90210'
+    }
+  };
 
+  constructor() { }
+  ngOnInit(): void { }
+  getEventValue(event: any) {
+    return event.target.value;
+  }
+  isNumeric(x: string | number) {
+    if (typeof x === 'undefined') return false;
+    return /^\d+$/.test(x.toString());
+  }
 }
